c02261a70aeacab916eb105a8115dacdb3c5e4db
nishida4e <nishidagijiroku@gmail.com>
2016-10-30 04:18:32 +0900

    clone

--- src/main/java/com/example/SpringtestloginApplication.java
+++ src/main/java/com/example/SpringtestloginApplication.java
@@ -9,4 +9,5 @@ public class SpringtestloginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestloginApplication.class, args);
 	}
+	
 }
---
+++ src/main/java/com/example/service/FoodPriceService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import com.example.domain.FoodPrice;
+import com.example.repository.FoodPriceJdbcRepository;
+import com.example.repository.FoodPriceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class FoodPriceService {
+
+	@Autowired
+	FoodPriceRepository fpRepo;
+	//FoodPriceJdbcRepository fpRepo;
+
+	public List<FoodPrice> findAll() {
+		return fpRepo.findAll();
+		//return fpRepo.findByNameContaining("カレー");
+	}
+
+	public FoodPrice findOne(Integer id) {
+		return fpRepo.findOne(id);
+	}
+
+	public FoodPrice create(FoodPrice fp) {
+		FoodPrice ret = fpRepo.save(fp);
+		// fpRepo.rollbacktest();
+		return ret;
+	}
+
+	public void delete(Integer id) {
+		fpRepo.delete(id);
+	}
+
+}
