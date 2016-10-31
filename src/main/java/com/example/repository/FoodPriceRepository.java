3bd87bcadfbba0afe3033e7ed327167a4d545aef
nishida4e <s_nishida@four-e.co.jp>
2016-10-31 21:02:50 +0900

    repository

--- src/main/java/com/example/repository/FoodPriceRepository.java
+++ src/main/java/com/example/repository/FoodPriceRepository.java
@@ -2,16 +2,6 @@ package com.example.repository;
 
 import com.example.domain.FoodPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface FoodPriceRepository extends JpaRepository<FoodPrice, Integer> {
-
-	@Query("SELECT a FROM FoodPrice a WHERE a.name LIKE %:name%")
-	List<FoodPrice> findByNameContaining(@Param("name") String name);
-
-	// http://docs.spring.io/spring-data/jpa/docs/current/reference/html/
-
 }
--- src/main/java/com/example/service/FoodPriceService.java
+++ src/main/java/com/example/service/FoodPriceService.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.domain.FoodPrice;
 import com.example.domain.User;
-import com.example.repository.FoodPriceJdbcRepository;
 import com.example.repository.FoodPriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,11 +15,9 @@ public class FoodPriceService {
 
 	@Autowired
 	FoodPriceRepository fpRepo;
-	//FoodPriceJdbcRepository fpRepo;
 
 	public List<FoodPrice> findAll() {
 		return fpRepo.findAll();
-		//return fpRepo.findByNameContaining("カレー");
 	}
 
 	public FoodPrice findOne(Integer id) {
